a454041d7e03d51f62f0aaa93d8048b019723f29
shifufx <mr.shifufx@gmail.com>
2023-01-11 10:48:08 +0700

    fixed new section :smiling_imp::smiling_imp::smiling_imp:

--- components/Section2.jsx
+++ components/Section2.jsx
@@ -9,7 +9,8 @@ const Section2 = () => {
         <h1 className='font-bold text-4xl py-12 text-center'> Course </h1>
 
         {/* grid */}
-        <div className="grid md:grid-cols2 lg:grid-cols-3 gap-14">
+        {/* <div className="grid md:grid-cols2 lg:grid-cols-3 gap-14"> */}
+        <div className="grid grid-flow-row gap-10 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
             { Post() }
             { Post() }
             { Post() }
@@ -22,29 +23,27 @@ const Section2 = () => {
     )
 }
 
-
 function Post(){
     return (
-        <div className="item ml-5 mt-5">
+        <div className="item mt-5 my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1">
             <div className="images">
                 {/* <Link href={"/"}><a> src={""} width={600} height={600} /></a> */}
                 <Link href={"/"}>
-                    <Image src={"/Batch/post-1.jpg"} width={310} height={280} className="rounded"/>
+                    <Image src={"/Batch/post-1.jpg"} width={310} height={280} className="rounded-t h-72 w-full object-cover"/>
                 </Link>
             </div>
 
-            <div className="info flex justify-center flex-col py-4">
-                <div className="cat">
-                    <Link href={"/"}></Link>
-                </div>
-                <div className="title text-left font-bold text-3xl">
+            <div className="info flex justify-center flex-col py-4 p-4">
+                <div className="title text-lg mb-4 font-bold leading-relaxed text-white dark:text-gray-300">
                     <Link href={"/"}><h1>Batch Panda</h1></Link>
                 </div>
-                <p className='text-gray-500 py-3'>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi maiores repellat magnam. Rem deserunt sapiente nisi totam quo illum repudiandae iure reprehenderit ipsam excepturi. Possimus, sunt iure. Ea, similique asperiores.
-                </p>
             </div>
+        
+        <Link href={"/contact"}>
+            <p className='p-4 text-white font-bold'>Download</p>
+        </Link>
         </div>
+        
     )
 }
 
